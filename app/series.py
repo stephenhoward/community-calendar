429e99bc6e454e92647d8561a376bb1a8f169872
Stephen Howard <stephen@enterity.com>
2019-12-01 08:41:54 -0800

    initial add of handlers for event series

---
+++ app/series.py
@@ -0,0 +1,12 @@
+from event_calendar.model.series import Series
+from event_calendar.model.image import SeriesImage
+import app.handlers as handlers
+
+search = handlers.search_for(Series)
+get    = handlers.get_for(Series)
+post   = handlers.post_for(Series)
+update = handlers.update_for(Series)
+delete = handlers.delete_for(Series)
+
+post_image = handlers.upload_file_for(SeriesImage)
+get_image  = handlers.serve_file_for(SeriesImage)
